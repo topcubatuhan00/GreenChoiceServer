35284be970c7ab56bcfd352f864bd03906ed77e1
Batuhan Topçu <topcubatuhan00@hotmail.com>
2023-08-08 15:27:11 +0300

    all controllers added

--- GreenChoice.WebApi/Controllers/StoreController.cs
+++ GreenChoice.WebApi/Controllers/StoreController.cs
@@ -1,6 +1,69 @@
-﻿namespace GreenChoice.WebApi.Controllers
+﻿using GreenChoice.Application.Services;
+using GreenChoice.Domain.Models.CommentModels;
+using GreenChoice.Domain.Models.HelperModels;
+using GreenChoice.Domain.Models.StoreModels;
+using GreenChoice.WebApi.CustomControllerBase;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenChoice.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class StoreController : CustomBaseController
 {
-    public class StoreController
+    #region Fields
+    private readonly IStoreService _storeService;
+    #endregion
+
+    #region Ctor
+    public StoreController(IStoreService storeService)
     {
+        _storeService = storeService;
     }
+    #endregion
+
+    #region List
+    [HttpGet("[action]")]
+    public async Task<IActionResult> GetAll([FromQuery] PaginationRequest request)
+    {
+        var categories = await _storeService.GetAll(request);
+
+        return CreateActionResultInstance(categories);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var category = await _storeService.GetById(id);
+
+        return CreateActionResultInstance(category);
+    }
+    #endregion
+
+    #region Create
+    [HttpPost("[action]")]
+    public async Task<IActionResult> Create(CreateStoreModel model)
+    {
+        await _storeService.Create(model);
+        return Ok(model);
+    }
+    #endregion
+
+    #region Update
+    [HttpPut("[action]")]
+    public async Task<IActionResult> Update(UpdateStoreModel model)
+    {
+        await _storeService.Update(model);
+        return Ok();
+    }
+    #endregion
+
+    #region Delete
+    [HttpDelete("(id)")]
+    public async Task<IActionResult> Remove(int id)
+    {
+        await _storeService.Remove(id);
+        return Ok();
+    }
+    #endregion
 }
--- GreenChoice.WebApi/Controllers/UserController.cs
+++ GreenChoice.WebApi/Controllers/UserController.cs
@@ -1,6 +1,69 @@
-﻿namespace GreenChoice.WebApi.Controllers
+﻿using GreenChoice.Application.Services;
+using GreenChoice.Domain.Models.CommentModels;
+using GreenChoice.Domain.Models.HelperModels;
+using GreenChoice.Domain.Models.UserModels;
+using GreenChoice.WebApi.CustomControllerBase;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenChoice.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : CustomBaseController
 {
-    public class UserController
+    #region Fields
+    private readonly IUserService _userService;
+    #endregion
+
+    #region Ctor
+    public UserController(IUserService userService)
     {
+        _userService = userService;
     }
+    #endregion
+
+    #region List
+    [HttpGet("[action]")]
+    public async Task<IActionResult> GetAll([FromQuery] PaginationRequest request)
+    {
+        var categories = await _userService.GetAll(request);
+
+        return CreateActionResultInstance(categories);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var category = await _userService.GetById(id);
+
+        return CreateActionResultInstance(category);
+    }
+    #endregion
+
+    #region Create
+    [HttpPost("[action]")]
+    public async Task<IActionResult> Create(CreateUserModel model)
+    {
+        await _userService.Create(model);
+        return Ok(model);
+    }
+    #endregion
+
+    #region Update
+    [HttpPut("[action]")]
+    public async Task<IActionResult> Update(UpdateUserModel model)
+    {
+        await _userService.Update(model);
+        return Ok();
+    }
+    #endregion
+
+    #region Delete
+    [HttpDelete("(id)")]
+    public async Task<IActionResult> Remove(int id)
+    {
+        await _userService.Remove(id);
+        return Ok();
+    }
+    #endregion
 }
